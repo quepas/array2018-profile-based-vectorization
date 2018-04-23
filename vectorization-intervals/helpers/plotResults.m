545a1385d444cd2a73f30bb22c5bdcde7c911fcd
quepas <kiepas@agh.edu.pl>
2018-04-23 19:54:39 +0200

    Removing more old codes

--- vectorization-intervals/helpers/plotResults.m
+++
@@ -1,5 +0,0 @@
-function plotResults(parameters, measurements, resultName, dirName)
-   plot(parameters, measurements);
-   legend('Original', 'Mc2Mc', 'HHM');
-   print([dirName, resultName, '_', version('-release')],'-dpng');
-end
--- vectorization-intervals/helpers/writeResults.m
+++
@@ -1,4 +0,0 @@
-function writeResults(parameters, data, resultName, dirName)
-   data = [parameters' data];
-   csvwrite([dirName, resultName, '_', version('-release'), '.csv'], data);
-end
