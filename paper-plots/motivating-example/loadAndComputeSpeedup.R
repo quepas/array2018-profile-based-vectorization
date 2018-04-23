4c1307b3ea2712673d6b8bfb102bd5cbf85292a5
quepas <kiepas@agh.edu.pl>
2018-04-23 16:23:09 +0200

    Proper column names

--- paper-plots/motivating-example/loadAndComputeSpeedup.R
+++ paper-plots/motivating-example/loadAndComputeSpeedup.R
@@ -1,9 +1,9 @@
 loadAndComputeSpeedup <- function(filename) {
   data <- read.csv(filename, header = FALSE);
-  colnames(data) <- c("sample", "loop", "lcpc", "hhm")
+  colnames(data) <- c("sample", "loop", "mc2mc", "mc2mc.optimized")
   x <- data$sample
   loopFit <- loess(loop ~ sample, data)
-  lcpcFit <- loess(lcpc ~ sample, data)
-  suLCPC <- predict(loopFit, x) / predict(lcpcFit, x)
-  return(list(sample=x, su=suLCPC))
+  mc2mcFit <- loess(mc2mc ~ sample, data)
+  suMC2MC <- predict(loopFit, x) / predict(mc2mcFit, x)
+  return(list(sample=x, su=suMC2MC))
 }
