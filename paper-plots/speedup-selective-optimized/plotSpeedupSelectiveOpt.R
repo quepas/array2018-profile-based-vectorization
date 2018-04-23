55f3bb399ef6028d650c73cc1a1e456ba9ee051e
quepas <kiepas@agh.edu.pl>
2018-04-23 17:10:24 +0200

    Adding plot for mc2mc selective optizmied code

---
+++ paper-plots/speedup-selective-optimized/plotSpeedupSelectiveOpt.R
@@ -0,0 +1,42 @@
+data <- read.csv("../../paper-results/speedup-selective-optimized/results.csv")
+# Aggregate the repeated measurements with mean value
+dataAggregate <- colMeans(data)
+# Compute speedups of optimized vectorized code with systematic and selective strategy
+speedups <- data.frame(backprop=c(dataAggregate["backprop_matlab"]/dataAggregate["backprop_mc2mc"],
+                                 dataAggregate["backprop_matlab"]/dataAggregate["backprop_mc2mc_selective"]),
+                      crni=c(dataAggregate["crni_matlab"]/dataAggregate["crni_mc2mc"],
+                             dataAggregate["crni_matlab"]/dataAggregate["crni_mc2mc_selective"]),
+                      fft=c(dataAggregate["fft_matlab"]/dataAggregate["fft_mc2mc"],
+                            dataAggregate["fft_matlab"]/dataAggregate["fft_mc2mc_selective"]),
+                      nw=c(dataAggregate["nw_matlab"]/dataAggregate["nw_mc2mc"],
+                           dataAggregate["nw_matlab"]/dataAggregate["nw_mc2mc_selective"]),
+                      pagerank=c(dataAggregate["pagerank_matlab"]/dataAggregate["pagerank_mc2mc"],
+                                 dataAggregate["pagerank_matlab"]/dataAggregate["pagerank_mc2mc_selective"]))
+# Save as pdf (A5 format)
+pdf("mc2mc-optimized.pdf", width = 8.27, height = 5.83)
+# Modify plot margins
+par(mfrow=c(1, 1), mar=c(5, 5, 2, 5))
+# Plot vectorization strategies comparison
+barplot(as.matrix(speedups), 
+        beside=T, 
+        axes = T,
+        xlab = "Benchmark",
+        ylab = "Speedup",
+        ylim= c(0, 2.0),
+        col=c("#f0f0f0","#bdbdbd"),
+        cex.names = 1.5,
+        cex.axis = 1.5,
+        cex.lab=1.5,
+        legend = c("Systematic", "Selective (optimized)"), 
+        args.legend = list(x ='topright',
+                           bty="n",
+                           cex=1.5,
+                           horiz=F,
+                           inset=c(0.15, -0.05),
+                           title=expression(bold(underline("Strategy"))), title.col="black"))
+# Plot baseline
+abline(a = 1, b= 0, col="gray", lty=5)
+# ... with a text !
+text(8.7, 1.06, "baseline", col="gray", cex=1.2)
+# Finish plotting
+dev.off()
--- paper-plots/speedup-selective/plotSpeedupSelective.R
+++ paper-plots/speedup-selective/plotSpeedupSelective.R
@@ -11,22 +11,22 @@ pdf("mc2mc-selective.pdf", width = 8.27, height = 5.83)
 # Modify plot margins
 par(mfrow=c(1, 1), mar=c(5, 10, 2, 13))
 # Plot vectorization strategies comparison
-b<-barplot(as.matrix(speedups),
-           beside=T,
-           axes = T,
-           col=c("#f0f0f0","#bdbdbd"),
-           xlab = "Benchmark",
-           ylab = "Speedup",
-           cex.names = 1.5,
-           cex.axis = 1.5,
-           cex.lab=1.5,
-           legend = c("Systematic", "Selective"),
-           args.legend = list(x ='right',
-                              bty='n',
-                              cex=1.5,
-                              horiz=F,
-                              inset=c(-0.6, 0),
-                              title=expression(bold(underline("Strategy"))), title.col="black"))
+barplot(as.matrix(speedups),
+        beside=T,
+        axes = T,
+        col=c("#f0f0f0","#bdbdbd"),
+        xlab = "Benchmark",
+        ylab = "Speedup",
+        cex.names = 1.5,
+        cex.axis = 1.5,
+        cex.lab=1.5,
+        legend = c("Systematic", "Selective"),
+        args.legend = list(x ='right',
+                           bty='n',
+                           cex=1.5,
+                           horiz=F,
+                           inset=c(-0.6, 0),
+                           title=expression(bold(underline("Strategy"))), title.col="black"))
 # Plot baseline
 abline(a = 1, b= 0, col="gray", lty=5)
 # ... with a text !
